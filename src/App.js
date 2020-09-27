47ae091de15671313b52f550e47488fb85152ae9
Jasmin Rowdy <jrowdy@wcpschools.wcpss.local>
2020-09-27 14:25:38 -0400

    changed data from hardcode to API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
+// import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,11 +8,16 @@ class App extends Component {
     constructor() {
         super()
         this.state= {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({robots: users}));
+    }
     onSearchChange = (event) => {
         this.setState({searchField: event.target.value})
     }
